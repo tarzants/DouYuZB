82a206c04785e8ebbed8b95eb400a58cc9f240b7
Sam <tarzants@gmail.com>
2019-06-03 16:09:55 +0800

    首页导航栏搭建

--- DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
+++ DouYuZB/DouYuZB/Classes/Home/Controller/HomeViewController.swift
@@ -26,10 +26,32 @@ extension HomeViewController {
         setupNavigationBar()
     }
     private func setupNavigationBar() {
-        let btn = UIButton()
-        btn.setImage(UIImage(named: "logo"), for: .normal)
-        btn.sizeToFit()
+        //设置左侧button
+        /*
+        let logobtn = UIButton()
+        logobtn.setImage(UIImage(named: "logo"), for: .normal)
+        logobtn.sizeToFit()
+        */
+        navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "logo")
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: btn)
+        //设置右侧button
+        let size = CGSize(width: 30, height: 35)
+
+        //构造函数方法
+        let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highimageName: "Image_scan_click", size: size)
+        
+        let searchItem = UIBarButtonItem(imageName: "btn_search", highimageName: "btn_search_clicked", size: size)
+        
+        let historyItem = UIBarButtonItem(imageName: "image_my_history", highimageName: "Image_my_history_click" , size: size)
+        /*
+        //抽象类函数
+        let qrcodeItem = UIBarButtonItem.creatItem(imageName: "Image_scan", highimageName: "Image_scan_click", size: size)
+        
+        let searchItem = UIBarButtonItem.creatItem(imageName: "btn_search", highimageName: "btn_search_clicked", size: size)
+        
+        let historyItem = UIBarButtonItem.creatItem(imageName: "image_my_history", highimageName: "Image_my_history_click" , size: size)
+        */
+        
+        navigationItem.rightBarButtonItems = [historyItem, searchItem, qrcodeItem]
     }
 }
--- DouYuZB/DouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
+++ DouYuZB/DouYuZB/Classes/Tools/Extension/UIBarButtonItem-Extension.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2019 Sam. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIBarButtonItem
+{
+    /*
+    //抽象类方法
+    class func creatItem(imageName : String, highimageName : String, size : CGSize) -> UIBarButtonItem {
+        let btn = UIButton()
+        
+        btn.setImage(UIImage(named: imageName), for: .normal)
+        btn.setImage(UIImage(named: highimageName), for: .highlighted)
+        btn.frame = CGRect(origin: .zero, size: size)
+        
+        return UIBarButtonItem(customView: btn)
+        
+    }
+    */
+    
+    //抽象便利构造函数方法
+    convenience init(imageName : String, highimageName : String = "", size : CGSize = CGSize.zero) {
+        let btn = UIButton()
+        
+        btn.setImage(UIImage(named: imageName), for: .normal)
+        if highimageName != "" {
+        btn.setImage(UIImage(named: highimageName), for: .highlighted)
+        }
+        if size == CGSize.zero {
+            btn.sizeToFit()
+        } else {
+        btn.frame = CGRect(origin: .zero, size: size)
+        }
+        
+        self.init(customView: btn)
+    }
+    
+}
